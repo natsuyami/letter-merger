8a70606969401ba325813f21f990820121b2dde1
shiranui0317 <rgbunquin@itcdcorporation.com>
2019-02-12 18:41:06 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatNativeDateModule} from '@angular/material';
+import {MaterialModule} from './material-module';
+
 import { AppComponent } from './app.component';
+import { HintComponent } from './components/hint/hint.component';
+import { GameComponent } from './page/game/game.component';
+import { LetterComponent } from './components/letter/letter.component';
+import { GuessComponent } from './components/form/guess/guess.component';
+import { MessageComponent } from './components/modal/message/message.component';
+import { InstructionComponent } from './components/modal/instruction/instruction.component';
+import { CrosswordComponent } from './components/crossword/crossword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HintComponent,
+    GameComponent,
+    LetterComponent,
+    GuessComponent,
+    MessageComponent,
+    InstructionComponent,
+    CrosswordComponent,
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
