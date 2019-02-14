605493e9192cde5ae65212279c340238baeaa5cc
shiranui0317 <rgbunquin@itcdcorporation.com>
2019-02-14 14:36:13 +0800

    root permission

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/letter-merger'));
+app.use(express.static('./dist/'));
 
 app.get('/*', function(req,res) {
     
